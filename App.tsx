0b4c7a707e240132856b38d5cc7226415a5cfe17
Lucas Dantas <lucasdantasmoura01@gmail.com>
2021-08-07 14:40:07 -0300

    Adicionando gradiente ao background

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import {Background} from './src/components/Background';
 import {SignIn} from './src/screens/SignIn';
 
 const App = () => {
@@ -9,14 +10,14 @@ const App = () => {
   }, []);
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SignIn />
-    </>
+    </Background>
   );
 };
 
